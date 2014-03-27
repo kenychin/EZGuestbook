8a6bdd09eeb440f879783f58a53a8248433795db
kenychin <kenychin@gmail.com>
2014-03-26 20:39:48 -0700

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,19 +1,18 @@
 <?php
-            //Variables for connecting to your database.
-            //These variable values come from your hosting account.
-            $hostname = "GuestbookPhp1.db.8376326.hostedresource.com";
-            $username = "GuestbookPhp1";
-            $dbname = "GuestbookPhp1";
+	//Variables for connecting to your database.
+	//These variable values come from your hosting account.
+	$hostname = "GuestbookPhp1.db.8376326.hostedresource.com";
+	$username = "GuestbookPhp1";
+	$dbname = "GuestbookPhp1";
 
-            //These variable values need to be changed by you before deploying
-            $password = "password";
-            $usertable = "messages";
-            $yourfield = "name";
-        
-            //Connecting to your database
-            mysql_connect($hostname, $username, $password) OR DIE ("Unable to 
-            connect to database! Please try again later.");
-	        	mysql_query("SET NAMES 'utf8'");
-            mysql_select_db($dbname);
+	//These variable values need to be changed by you before deploying
+	$password = "";
+	$usertable = "messages";
+	$yourfield = "name";
+
+	//Connecting to your database
+	mysql_connect($hostname, $username, $password) OR DIE ("Unable to connect to database! Please try again later.");
+	mysql_query("SET NAMES 'utf8'");
+	mysql_select_db($dbname);
 
 ?>
